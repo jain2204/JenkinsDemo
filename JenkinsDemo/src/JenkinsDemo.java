f63de2101cf1087bd7137090eedc03d0650c8518
ashishAppirio <jainashish2204@gmail.com>
2020-05-19 22:26:05 +0530

    first commit

---
+++ JenkinsDemo/src/JenkinsDemo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class JenkinsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("test number");
+		
+		int n = sc.nextInt();
+		
+		if(n > 5) {
+			System.out.println("Number is greater than 5 >>>>>>>" +n);
+			
+		}else {
+			System.out.println("number is less than 5 >>>>>>>" +n);
+		}
+
+	}
+
+}
