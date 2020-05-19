fc7671b84cec08778dde1af68ba4ff433779fd71
ashishAppirio <ashish.jain@appirio.com>
2020-05-19 22:31:00 +0530

    test commit

--- JenkinsDemo/src/JenkinsDemo.java
+++ JenkinsDemo/src/JenkinsDemo.java
@@ -8,7 +8,7 @@ public class JenkinsDemo {
 
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("enter number ");
+		System.out.println("enter any number");
 		
 		int n = sc.nextInt();
 		
